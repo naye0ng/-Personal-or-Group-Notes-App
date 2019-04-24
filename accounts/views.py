f2bab7b3fe9abb639d1178eedcd4307da32ac7d7
naye0ng <nayeong_e@naver.com>
2019-04-24 05:36:11 +0000

    add login template and function

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+
 
 # Create your views here.
 def login(request) :
-    pass
+    if request.method == 'POST' :
+        form = AuthenticationForm(request.POST)
+        if form.is_valid() :
+            # 로그인하여 원래 주소로 넘겨주기
+            auth_login(request, form.get_user())
+            return redirect(request.GET.get('next') or 'posts:list')
+
+    form = AuthenticationForm()
+    content = {
+        'form' :form,
+    }
+    return render(request,'accounts/login.html',content)
 
 def logout(request) :
     pass
