ff1f35a4ce9bb20519d625bedf1c99098b6e118f
naye0ng <nayeong_e@naver.com>
2019-04-24 05:37:07 +0000

    Profile model 생성

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Profile(models.Model) :
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    nickname = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+    image = models.ImageField(blank=True)
